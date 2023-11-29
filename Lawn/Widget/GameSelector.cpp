f03659dfadb3f0c3ae882876c32c5c14323cc187
Aaron Cottle <aaron@aaroncottle.com.au>
2023-11-29 11:32:52 +1100

    Got the mouse cursor rendering and also the trophy rendering order fixed.

--- Lawn/Widget/GameSelector.cpp
+++ Lawn/Widget/GameSelector.cpp
@@ -127,7 +127,7 @@ GameSelector::GameSelector(LawnApp *theApp) {
         Sexy::IMAGE_SELECTORSCREEN_ACHIEVEMENTS_PEDESTAL->mHeight
     );
     mAchievementsButton->mClip = false;
-    mAchievementsButton->mBtnNoDraw = false;
+    mAchievementsButton->mBtnNoDraw = mHasTrophy;
     mAchievementsButton->mMouseVisible = false;
 
     mQuickPlayButton = MakeNewButton(
@@ -278,7 +278,7 @@ GameSelector::GameSelector(LawnApp *theApp) {
     TodHesitationTrace("gameselectorinit");
 }
 
-// 0x449D00¡¢0x449D20
+// 0x449D00ã€0x449D20
 GameSelector::~GameSelector() {
     if (mAdventureButton) delete mAdventureButton;
     if (mMinigameButton) delete mMinigameButton;
@@ -493,24 +493,6 @@ void GameSelector::Draw(Graphics *g) {
             g, Sexy::FONT_BRIANNETOD16, aOverlayMatrix * aOffsetMatrix, aWelcomeStr, Color(255, 245, 200)
         );
     }
-
-    int aLeftIdx = aSelectorReanim->FindTrackIndex("SelectorScreen_BG_Left");
-    ReanimatorTransform aTransformLeft;
-    aSelectorReanim->GetCurrentTransform(aLeftIdx, &aTransformLeft);
-    if (mHasTrophy) {
-        // @Patoke: updated pos to match GOTY
-        if (mApp->EarnedGoldTrophy())
-            TodDrawImageCelF(
-                g, Sexy::IMAGE_SUNFLOWER_TROPHY, aTransformLeft.mTransX + 12.f, aTransformLeft.mTransY + 345.f, 1, 0
-            );
-        else
-            TodDrawImageCelF(
-                g, Sexy::IMAGE_SUNFLOWER_TROPHY, aTransformLeft.mTransX + 12.f, aTransformLeft.mTransY + 345.f, 0, 0
-            );
-
-        TodParticleSystem *aTrophyParticle = mApp->ParticleTryToGet(mTrophyParticleID);
-        if (aTrophyParticle) aTrophyParticle->Draw(g);
-    }
 }
 
 // 0x44AB50
@@ -538,8 +520,8 @@ void GameSelector::DrawOverlay(Graphics *g) {
         float aTransSubX = aTransAreaX;
         float aTransSubY = aTransAreaY;
 
-        int aStage = ClampInt((mLevel - 1) / 10 + 1, 1, 6); // ´ó¹Ø
-        int aSub = mLevel - (aStage - 1) * 10;              // Ð¡¹Ø
+        int aStage = ClampInt((mLevel - 1) / 10 + 1, 1, 6); // å¤§å…³
+        int aSub = mLevel - (aStage - 1) * 10;              // å°å…³
         if (mApp->IsTrialStageLocked() && (mLevel >= 25 || mApp->HasFinishedAdventure())) {
             aStage = 3;
             aSub = 4;
@@ -559,7 +541,7 @@ void GameSelector::DrawOverlay(Graphics *g) {
         // @Patoke: changed positions for GOTY adventure icon
         TodDrawImageCelF(
             g, Sexy::IMAGE_SELECTORSCREEN_LEVELNUMBERS, aTransAreaX + 486.0f, aTransAreaY + 47.f, aStage, 0
-        ); // »æÖÆ´ó¹ØÊý
+        ); // ç»˜åˆ¶å¤§å…³æ•°
         if (aSub < 10) {
             TodDrawImageCelF(
                 g, Sexy::IMAGE_SELECTORSCREEN_LEVELNUMBERS, aTransSubX + 509.f, aTransSubY + 50.f, aSub, 0
@@ -592,6 +574,27 @@ void GameSelector::DrawOverlay(Graphics *g) {
 
         if (gIsPartnerBuild) g->DrawString(_S("PRESS/PARTNER PREVIEW BUILD: DO NOT DISTRIBUTE"), 27, 594);
         else g->DrawString(_S("BETA BUILD: DO NOT DISTRIBUTE"), 27, 594);
+    }
+
+    // @Minerscale: Trophy needs to draw in the DrawOverlay
+    Reanimation *aSelectorReanim = mApp->ReanimationGet(mSelectorReanimID);
+
+    int aLeftIdx = aSelectorReanim->FindTrackIndex("SelectorScreen_BG_Left");
+    ReanimatorTransform aTransformLeft;
+    aSelectorReanim->GetCurrentTransform(aLeftIdx, &aTransformLeft);
+    if (mHasTrophy) {
+        // @Patoke: updated pos to match GOTY
+        if (mApp->EarnedGoldTrophy())
+            TodDrawImageCelF(
+                g, Sexy::IMAGE_SUNFLOWER_TROPHY, aTransformLeft.mTransX + 12.f, aTransformLeft.mTransY + 345.f, 1, 0
+            );
+        else
+            TodDrawImageCelF(
+                g, Sexy::IMAGE_SUNFLOWER_TROPHY, aTransformLeft.mTransX + 12.f, aTransformLeft.mTransY + 345.f, 0, 0
+            );
+
+        TodParticleSystem *aTrophyParticle = mApp->ParticleTryToGet(mTrophyParticleID);
+        if (aTrophyParticle) aTrophyParticle->Draw(g);
     }
 
     mToolTip->Draw(g);
