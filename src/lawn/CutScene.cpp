b5c776b4f2c6849a861682b81f71f20cd1f2c2b2
Aaron Cottle <aaron@aaroncottle.com.au>
2024-02-16 01:13:05 +1100

    fix tree of wisdom invisible shovel

--- src/lawn/CutScene.cpp
+++ src/lawn/CutScene.cpp
@@ -1133,6 +1133,7 @@ void CutScene::ShowShovel() {
         mApp->mGameMode == GameMode::GAMEMODE_CHALLENGE_BEGHOULED ||
         mApp->mGameMode == GameMode::GAMEMODE_CHALLENGE_BEGHOULED_TWIST ||
         mApp->mGameMode == GameMode::GAMEMODE_CHALLENGE_ZEN_GARDEN ||
+        mApp->mGameMode == GameMode::GAMEMODE_TREE_OF_WISDOM ||
         mApp->mGameMode == GameMode::GAMEMODE_CHALLENGE_ZOMBIQUARIUM || mApp->IsIZombieLevel())
         return;
 
